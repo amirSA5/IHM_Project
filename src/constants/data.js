dd7226f2b572d90af74505717bc9b0a6aee29fc1
amirSA5 <amir434soussi@gmail.com>
2023-05-02 13:31:17 +0100

    first commit

---
+++ src/constants/data.js
@@ -0,0 +1,7 @@
+export const categories = [
+    { id: 1, type: "Social" },
+    { id: 2, type: "Familial" },
+    { id: 3, type: "Professional Life" },
+    { id: 4, type: "Education" },
+    { id: 5, type: "Childhood" }
+];
