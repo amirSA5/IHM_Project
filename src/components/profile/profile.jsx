614a2de01a06c922be4ff06274fea80d693085b3
Omar Talbi <talbiomar2012@gmail.com>
2023-05-04 12:34:47 +0100

    added profile page

---
+++ src/components/profile/profile.jsx
@@ -0,0 +1,212 @@
+import { Box, styled, Typography, Link } from "@mui/material";
+import { GitHub, Instagram, Email } from "@mui/icons-material";
+import { profileStyle } from "./profileStyle.css";
+
+const Banner = styled(Box)`
+  background-image: url(https://www.wallpapertip.com/wmimgs/23-236943_us-wallpaper-for-website.jpg);
+  width: 100%;
+  height: 50vh;
+  background-position: left 0px bottom 0px;
+  background-size: cover;
+`;
+
+const Wrapper = styled(Box)`
+  padding: 20px;
+  & > h3,
+  & > h5 {
+    margin-top: 50px;
+  }
+`;
+
+const Text = styled(Typography)`
+  color: #878787;
+`;
+
+const Profile = () => {
+  return (
+    <div>
+      <div class="container">
+        <div class="row gutters">
+          <div class="col-xl-3 col-lg-3 col-md-12 col-sm-12 col-12">
+            <div class="card h-100">
+              <div class="card-body">
+                <div class="account-settings">
+                  <div class="user-profile">
+                    <div class="user-avatar">
+                      <img
+                        src="https://bootdey.com/img/Content/avatar/avatar7.png"
+                        alt="Maxwell Admin"
+                      />
+                    </div>
+                    <h5 class="user-name">Omar Talbi</h5>
+                    <h6 class="user-email">
+                      <a
+                        href="/cdn-cgi/l/email-protection"
+                        class="__cf_email__"
+                        data-cfemail="671e120c0e272a061f10020b0b4904080a"
+                      >
+                        [email&#160;protected]
+                      </a>
+                    </h6>
+                  </div>
+                  <div class="about">
+                    <h5>About</h5>
+                    <p>
+                      this is a description of the user that owns this profile.
+                    </p>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div class="col-xl-9 col-lg-9 col-md-12 col-sm-12 col-12">
+            <div class="card h-100">
+              <div class="card-body">
+                <div class="row gutters">
+                  <div class="col-xl-12 col-lg-12 col-md-12 col-sm-12 col-12">
+                    <h6 class="mb-2 text-primary">Personal Details</h6>
+                  </div>
+                  <div class="col-xl-6 col-lg-6 col-md-6 col-sm-6 col-12">
+                    <div class="form-group">
+                      <label for="fullName">Full Name</label>
+                      <input
+                        type="text"
+                        class="form-control"
+                        id="fullName"
+                        placeholder="Enter full name"
+                      />
+                    </div>
+                  </div>
+                  <div class="col-xl-6 col-lg-6 col-md-6 col-sm-6 col-12">
+                    <div class="form-group">
+                      <label for="eMail">Email</label>
+                      <input
+                        type="email"
+                        class="form-control"
+                        id="eMail"
+                        placeholder="Enter email ID"
+                      />
+                    </div>
+                  </div>
+                  <div class="col-xl-6 col-lg-6 col-md-6 col-sm-6 col-12">
+                    <div class="form-group">
+                      <label for="phone">Phone</label>
+                      <input
+                        type="text"
+                        class="form-control"
+                        id="phone"
+                        placeholder="Enter phone number"
+                      />
+                    </div>
+                  </div>
+                  <div class="col-xl-6 col-lg-6 col-md-6 col-sm-6 col-12">
+                    <div class="row">
+                      <div class="col-12 d-flex align-items-center">
+                        <label for="switch">
+                          Select user profile type anonymous or public
+                        </label>
+                      </div>
+                      <div class="col-12 align-items-center">
+                        <div id="app-cover">
+                          <div class="row">
+                            <div class="toggle-button-cover">
+                              <div class="button r" id="button-1">
+                                <input type="checkbox" class="checkbox" />
+                                <div class="knobs"></div>
+                                <div class="layer"></div>
+                              </div>
+                            </div>
+                          </div>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+                <div class="row gutters">
+                  <div class="col-xl-12 col-lg-12 col-md-12 col-sm-12 col-12">
+                    <h6 class="mt-3 mb-2 text-primary">Address</h6>
+                  </div>
+                  <div class="col-xl-6 col-lg-6 col-md-6 col-sm-6 col-12">
+                    <div class="form-group">
+                      <label for="Street">Street</label>
+                      <input
+                        type="name"
+                        class="form-control"
+                        id="Street"
+                        placeholder="Enter Street"
+                      />
+                    </div>
+                  </div>
+                  <div class="col-xl-6 col-lg-6 col-md-6 col-sm-6 col-12">
+                    <div class="form-group">
+                      <label for="ciTy">City</label>
+                      <input
+                        type="name"
+                        class="form-control"
+                        id="ciTy"
+                        placeholder="Enter City"
+                      />
+                    </div>
+                  </div>
+                  <div class="col-xl-6 col-lg-6 col-md-6 col-sm-6 col-12">
+                    <div class="form-group">
+                      <label for="sTate">State</label>
+                      <input
+                        type="text"
+                        class="form-control"
+                        id="sTate"
+                        placeholder="Enter State"
+                      />
+                    </div>
+                  </div>
+                  <div class="col-xl-6 col-lg-6 col-md-6 col-sm-6 col-12">
+                    <div class="form-group">
+                      <label for="zIp">Zip Code</label>
+                      <input
+                        type="text"
+                        class="form-control"
+                        id="zIp"
+                        placeholder="Zip Code"
+                      />
+                    </div>
+                  </div>
+                </div>
+                <div class="row gutters">
+                  <div class="col-xl-12 col-lg-12 col-md-12 col-sm-12 col-12">
+                    <div class="text-right">
+                      <button
+                        type="button"
+                        id="submit"
+                        name="submit"
+                        class="btn btn-secondary"
+                      >
+                        Cancel
+                      </button>
+                      <button
+                        type="button"
+                        id="submit"
+                        name="submit"
+                        class="btn btn-primary"
+                      >
+                        Update
+                      </button>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+      <script
+        data-cfasync="false"
+        src="/cdn-cgi/scripts/5c5dd728/cloudflare-static/email-decode.min.js"
+      ></script>
+      <script src="https://code.jquery.com/jquery-1.10.2.min.js"></script>
+      <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.5.0/dist/js/bootstrap.bundle.min.js"></script>
+      <script type="text/javascript"></script>
+    </div>
+  );
+};
+
+export default Profile;
